e499f9508601c94d6930c2f3ac9ee49f25aa29e4
prao582 <prao582@aucklanduni.ac.nz>
2021-10-05 20:58:39 +1300

    finished solver ver1.0
    why is it infeasible??????

--- Woolworths LP.py
+++ Woolworths LP.py
@@ -1,8 +1,13 @@
 
 
 import numpy as np
+from numpy.core.numeric import NaN
 import pandas as pd 
 from pulp import *
+
+np.set_printoptions(threshold=sys.maxsize)
+
+
 
 
 
@@ -13,19 +18,7 @@ def load_data():
     storeLocations = pd.read_csv("WoolworthsDemands.csv", usecols = [0])
     return Weekday_Routes, Weekend_Routes, storeLocations
 
-
-
-def solve_lp(routeData, storeLocations): 
-    
-    
-    
-    #Get index for the df
-    routes = routeData.index
-    #print(routeData)
-    durations = routeData['Duration']
-    costs = durations.multiply(0.0625)
-
-    print(routeData)
+def column_generation(routeData, storeLocations):
 
     firstStops = routeData.to_dict()['First Stop']
     secondStops = routeData.to_dict()['Second Stop']
@@ -33,67 +26,85 @@ def solve_lp(routeData, storeLocations):
     fourthStops = routeData.to_dict()['Fourth Stop']
     storeLocationDict = storeLocations.to_dict()['Store']
 
-    routeVisits = np.zeros( (len(firstStops), len(storeLocations)) )
+    routeVisits = np.zeros( (len(storeLocations), len(firstStops)) )
 
-
-    
-
-
+    #pulls the location number from a route number 
+    locationNumber = list(storeLocationDict.keys())[list(storeLocationDict.values()).index(firstStops[160])]
 
     for i in range(len(firstStops)):
-        for j in range(len(storeLocations)):
-            routeNumber = list(storeLocationDict.keys())[list(storeLocationDict.values()).index(firstStops[i])]
-            routeVisits[i][routeNumber] = 1   
+        #pulls the location number from a route number
+        if pd.isna(firstStops[i]) is False:
+            locationNumber = list(storeLocationDict.keys())[list(storeLocationDict.values()).index(firstStops[i])]
+            routeVisits[locationNumber][i] = 1   
+    
+    for i in range(len(secondStops)):
+        #pulls the location number from a route number 
+        if pd.isna(secondStops[i]) is False:
+            locationNumber = list(storeLocationDict.keys())[list(storeLocationDict.values()).index(secondStops[i])]
+            routeVisits[locationNumber][i] = 1  
+    
+    for i in range(len(thirdStops)):
+        #pulls the location number from a route number 
+        if pd.isna(thirdStops[i]) is False:
+            locationNumber = list(storeLocationDict.keys())[list(storeLocationDict.values()).index(thirdStops[i])]
+            routeVisits[locationNumber][i] = 1  
+    
+    for i in range(len(fourthStops)):
+        #pulls the location number from a route number
+        if pd.isna(fourthStops[i]) is False:
+            locationNumber = list(storeLocationDict.keys())[list(storeLocationDict.values()).index(fourthStops[i])]
+            routeVisits[locationNumber][i] = 1  
 
-    print(routeVisits)   
-    
+    return routeVisits
 
+def solve_lp(routeData, storeLocations): 
     
+    routeVisits = column_generation(routeData, storeLocations)
+    rows = np.shape(routeVisits)[0]
+    columns = np.shape(routeVisits)[1]
     
-    #get store columns for the df
-    #stores = pd.Series([str(x) for x in range(64 + 1)], index = routes)
+    #Get index for the df
+    routes = routeData.index
     
-    # routeData = pd.DataFrame({'routeNumber': routes, 
-    #                             'Stores': stores, })
-
-    #for i,x in enumerate(routes):
-        
-    
+    durations = routeData['Duration']
+    costs = durations.multiply(0.0625)
+    costs = costs.to_dict()
 
     #create prob variable object for problem data
-    #prob = LpProblem("Eastern Routes", LpMinimize)
+    prob = LpProblem("Routes", LpMinimize)
 
     #Dictionary containing route variables
-    #route_chosen = LpVariable.dicts("chosen", routes, 0, 1, cat = 'Integer')
+    route_chosen = LpVariable.dicts("chosen", routes, 0, 1, cat = 'Integer')
+
+    #input the obj function into prob using the profits for each tie type
+    prob +=lpSum([costs[i]*route_chosen[i] for i in routes]), "Objective cost function"
+
+    #constraint: each route only visits node once
+    for i in range(rows):
+        for j in range(columns):
+            prob += lpSum([route_chosen[j]]) == 1
+
+    #constraint: Trucks
+    prob += lpSum(route_chosen[i] for i in routes) <= 60
 
 
-    # #input the obj function into prob using the profits for eatch tie type
-    #prob +=lpSum([cost[i]*route_chosen[i] for i in easternRoutes.index]), "Objective cost function"
-
+        
     # for r in easternRoutes.index:
     # prob += easternRoutes.index[r]>= route_chosen[f]*0.1
     # prob += easternRoutes.index[r]<= route_chosen[f]*1e5
 
 
-
-
-
-
-
-
-
-
     ##SOLVING ROUTINES
-    #prob.writeLP('Routes.lp')
-    # prob.solve()
+    prob.writeLP('Routes.lp')
+    prob.solve()
 
     # The status of the solution is printed to the screen
-    # print("Status:", LpStatus[prob.status])
+    print("Status:", LpStatus[prob.status])
 
-    # for v in prob.variables():
-    #     print(v.name, "=", v.varValue)
+    for v in prob.variables():
+        print(v.name, "=", v.varValue)
    
-    # print("Optimised cost ", value(prob.objective))
+    print("Optimised cost ", value(prob.objective))
 
     # Each of the variables is printed with its resolved optimum value
 
@@ -121,12 +132,6 @@ if __name__ == "__main__":
     #print(easternRoutes[3]['First Stop'])
 
     
-    
-
-
-
-
-
-
     solve_lp(Weekday_Routes, storeLocations)
+
 
